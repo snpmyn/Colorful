f3b3500df4a62f0a982ca63e92dc6e61f9da569c
snpmyn <snpmyn@126.com>
2019-10-19 17:22:04 +0800

    优化

--- app/src/main/java/application/ColorfulApp.java
+++ app/src/main/java/application/ColorfulApp.java
@@ -1,9 +1,14 @@
 package application;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 
 import com.zsp.colorful.BuildConfig;
 import com.zsp.colorful.library.Colorful;
+import com.zsp.utilone.activity.ActivitySuperviseManager;
 import com.zsp.utilone.timber.configure.TimberInitConfigure;
 
 /**
@@ -26,6 +31,8 @@ public class ColorfulApp extends Application {
     private void initConfiguration() {
         // timber
         TimberInitConfigure.initTimber(BuildConfig.DEBUG);
+        // 全局监听Activity生命周期
+        registerActivityListener();
         // colorful
         Colorful.defaults()
                 .primaryColor(Colorful.ThemeColor.RED)
@@ -33,5 +40,49 @@ public class ColorfulApp extends Application {
                 .translucent(false)
                 .dark(true);
         Colorful.init(this);
+    }
+
+    /**
+     * Activity全局监听
+     */
+    private void registerActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
+                // 添监听到创事件Activity至集合
+                ActivitySuperviseManager.pushActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+                // 移监听到销事件Activity出集合
+                ActivitySuperviseManager.removeActivity(activity);
+            }
+        });
     }
 }
